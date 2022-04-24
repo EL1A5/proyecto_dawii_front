c5ba34cfc722f88b206bc44b01185b85f4f3653e
sebas0104 <sebastian.am345@gmail.com>
2022-04-23 19:52:38 -0500

    Insertar_Reclamo

--- src/app/components/registra-reclamo/registra-reclamo.component.ts
+++ src/app/components/registra-reclamo/registra-reclamo.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from 'src/app/models/cliente.model';
+import { Reclamo } from 'src/app/models/reclamo.model';
+import { TipoReclamo } from 'src/app/models/tipo-reclamo.model';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { ReclamoService } from 'src/app/services/reclamo.service';
+import { TipoReclamoService } from 'src/app/services/tipo-reclamo.service';
 
 @Component({
   selector: 'app-registra-reclamo',
@@ -6,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registra-reclamo.component.css']
 })
 export class RegistraReclamoComponent implements OnInit {
+ 
+  TipoReclamo : TipoReclamo[] = [];
+  Cliente  : Cliente[] = [];
+  reclamo: Reclamo ={
+    tipoReclamo:{
 
-  constructor() { }
+      idTipoReclamo: -1
+    },
 
+    cliente:{
+
+      idCliente: -1
+    }
+
+  };
+
+  constructor(private tipoReclamoService:TipoReclamoService,private ReclamoService:ReclamoService,private clienteService:ClienteService){
+    this.tipoReclamoService. listaTipoReclamo().subscribe(
+      (x)=> this.TipoReclamo=x
+      );
+      this.clienteService.listaCliente().subscribe(
+        (x)=> this.Cliente=x
+    );
+  }
+ 
+
+ insertado(){
+
+  
+    this.ReclamoService.insertaReclamo(this.reclamo).subscribe(
+     
+       (x)=> alert(x.mensaje)
+        
+    );
+   
+  }
+  
   ngOnInit(): void {
   }
 
--- src/app/services/reclamo.service.ts
+++ src/app/services/reclamo.service.ts
@@ -4,15 +4,24 @@ import { Observable } from 'rxjs';
 import { Reclamo } from '../models/reclamo.model';
 import { AppSettings } from '../app.settings';
 
-const baseUrlUtil = AppSettings.API_ENDPOINT+ '/util';
-const baseUrlReclamo = AppSettings.API_ENDPOINT+ '/reclamo';
+const baseUrlUtil = AppSettings.API_ENDPOINT+ 'util';
+const baseUrlReclamo = AppSettings.API_ENDPOINT+ 'reclamo';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReclamoService {
+  insertaRegistra(reclamo: Reclamo) {
+    throw new Error('Method not implemented.');
+  }
 
   constructor(private http:HttpClient) {   }
+
+  insertaReclamo(data:Reclamo):Observable<any>{
+  
+    return this.http.post(baseUrlReclamo, data)
+
+}
 
 }
