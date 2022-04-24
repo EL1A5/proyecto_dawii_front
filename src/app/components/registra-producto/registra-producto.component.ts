4bdea793708362db0a6fa650f8a43825f6df02fb
EL1A5 <provinciaelias@gmail.com>
2022-04-24 10:22:54 -0500

    registrar producto completo

--- src/app/components/registra-producto/registra-producto.component.ts
+++ src/app/components/registra-producto/registra-producto.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Marca } from 'src/app/models/marca.model';
+import { Pais } from 'src/app/models/pais.model';
+import { Producto } from 'src/app/models/producto.model';
+import { MarcaService } from 'src/app/services/marca.service';
+import { PaisService } from 'src/app/services/pais.service';
+import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
   selector: 'app-registra-producto',
@@ -6,8 +12,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registra-producto.component.css']
 })
 export class RegistraProductoComponent implements OnInit {
+   marcas:Marca  [ ]= [ ];
+   paises:Pais  [ ]= [ ];
 
-  constructor() { }
+   producto:Producto={
+     marca:{idMarca:0},
+     pais:{idPais:0}
+   };
+  constructor(private marcaService : MarcaService, private paisesService : PaisService ,private productService:ProductoService  ) { 
+
+    this.marcaService.listaMarca().subscribe(
+      (x)=>this.marcas=x
+    );
+    this.paisesService.listaPais().subscribe(
+      (x)=>this.paises=x
+    );
+
+  }
+
+  registrar(){
+    this.productService.registrarProducto(this.producto).subscribe(
+      response=>{
+        alert(response.mensaje);
+      },error=>{
+        alert(error);
+      }
+    );
+  }
 
   ngOnInit(): void {
   }
