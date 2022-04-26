4ccddda858a8a9e88dd5f767772596b1527bb400
AlexanderBern001 <bryan9112@hotmail.com>
2022-04-25 23:26:17 -0500

    registrar cliente completo

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -4,8 +4,7 @@ import { Observable } from 'rxjs';
 import { AppSettings } from '../app.settings';
 import { Cliente } from '../models/cliente.model';
 
-const baseUrlUtil = AppSettings.API_ENDPOINT+ '/util';
-const baseUrlCliente = AppSettings.API_ENDPOINT+ '/cliente';
+const baseUrlCliente = AppSettings.API_ENDPOINT+ '/rest/cliente';
 
 @Injectable({
   providedIn: 'root'
@@ -15,11 +14,13 @@ export class ClienteService {
   
   constructor(private http:HttpClient) { }
 
-  listaCliente():Observable<Cliente[]>{
-    return this.http.get<Cliente[]>(baseUrlUtil+"/listaCliente");
-  }
 
-  
 
-  
+  registraCliente(cliente: Cliente):Observable<any>{
+    return this.http.post<any>(baseUrlCliente, cliente);
 }
+
+}
+  
+
+
