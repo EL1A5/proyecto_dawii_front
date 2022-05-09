da1b5e6c6280e9402d5883740c600b6694b52f2b
AlexanderBern001 <bryan9112@hotmail.com>
2022-05-08 21:43:40 -0500

    Finalizado Consutar Cliente

--- src/app/components/consulta-cliente/consulta-cliente.component.ts
+++ src/app/components/consulta-cliente/consulta-cliente.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from 'src/app/models/cliente.model';
+import { Ubigeo } from 'src/app/models/ubigeo.model';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { UbigeoService } from 'src/app/services/ubigeo.service';
 
 @Component({
   selector: 'app-consulta-cliente',
@@ -7,9 +11,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultaClienteComponent implements OnInit {
 
-  constructor() { }
+  nombres:string="";
+  dni:string="";
+  selDepartamento:string = "-1"; 
+  selProvincia:string = "-1"; 
+  selDistrito:number = -1;
+  estado:boolean = true;
+  
 
-  ngOnInit(): void {
+  departamentos: string[]  = [];
+  provincias: string[]  = [];
+  distritos: Ubigeo[]  = [];
+
+ 
+  clientes: Cliente[] = [];
+
+  constructor(private ubigeoService: UbigeoService, private clienteService:ClienteService) { 
+
+    ubigeoService.listarDepartamento().subscribe(
+        (x) => this.departamentos = x
+    );
+
   }
 
+  consultaCliente(){
+    this.clienteService.listaClientes(this.nombres, this.dni, this.selDistrito, this.estado?1:0).subscribe(
+          (x) => {
+              this.clientes = x.lista;
+              alert(x.mensaje);
+          }
+    );
+}
+
+  cargaProvincia(){
+        this.ubigeoService.listaProvincias(this.selDepartamento).subscribe(
+                (x)  => this.provincias = x      
+        );
+        this.selProvincia = "-1";
+        this.distritos = [];
+        this.selDistrito = -1;
+  }
+  cargaDistrito(){
+        this.ubigeoService.listaDistritos(this.selDepartamento, this.selProvincia).subscribe(
+                (x)  => this.distritos = x      
+        );
+        this.selDistrito = -1;
+  }
+
+
+  ngOnInit(): void {}
 }
