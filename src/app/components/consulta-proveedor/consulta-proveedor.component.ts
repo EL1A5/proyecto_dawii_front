ffa308608d04140ee6ea5c0695c78d68266f5bf7
EPY <parionayauricasa@gmail.com>
2022-05-24 23:09:43 -0500

    PC2_ParionaYauricasa

--- src/app/components/consulta-proveedor/consulta-proveedor.component.ts
+++ src/app/components/consulta-proveedor/consulta-proveedor.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Proveedor } from 'src/app/models/proveedor.model';
+import { Ubigeo } from 'src/app/models/ubigeo.model';
+import { ProveedorService } from 'src/app/services/proveedor.service';
+import { UbigeoService } from 'src/app/services/ubigeo.service';
 
 @Component({
   selector: 'app-consulta-proveedor',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultaProveedorComponent implements OnInit {
 
-  constructor() { }
+    razonsocial: string = "";
+    ruc: string = "";
+    selDepartamento:string = "-1"; 
+    selProvincia:string = "-1"; 
+    selDistrito:number = -1;
+    estado:boolean = true;
 
-  ngOnInit(): void {
+    departamentos: string[]  = [];
+    provincias: string[]  = [];
+    distritos: Ubigeo[]  = [];
+    proveedor: Proveedor[] = [];
+
+  constructor(private ubigeoService: UbigeoService, private proveedorService:ProveedorService) {
+
+    ubigeoService.listarDepartamento().subscribe(
+      (x) => this.departamentos = x
+    );
+   }
+ 
+ consultaProveedor(){
+    this.proveedorService.listaProveedores(this.razonsocial, this.ruc, this.selDistrito, this.estado?1:0).subscribe(
+          (x) => {
+              this.proveedor = x.lista;
+              alert(x.mensaje);
+          }
+    );
+}
+
+  cargaProvincia(){
+        this.ubigeoService.listaProvincias(this.selDepartamento).subscribe(
+                (x)  => this.provincias = x      
+        );
+        this.selProvincia = "-1";
+        this.distritos = [];
+        this.selDistrito = -1;
   }
+  cargaDistrito(){
+        this.ubigeoService.listaDistritos(this.selDepartamento, this.selProvincia).subscribe(
+                (x)  => this.distritos = x      
+        );
+        this.selDistrito = -1;
+  }
+
+  ngOnInit(): void {}
+
 
 }
--- src/app/services/proveedor.service.ts
+++ src/app/services/proveedor.service.ts
@@ -5,7 +5,7 @@ import { Proveedor } from '../models/proveedor.model';
 import { AppSettings } from '../app.settings';
 
 const baseUrlUtil = AppSettings.API_ENDPOINT+ '/util';
-const baseUrlProveedor = AppSettings.API_ENDPOINT+ '/proveedor';
+const baseUrlProveedor = AppSettings.API_ENDPOINT+ '/rest/proveedor';
 
 
 @Injectable({
@@ -14,8 +14,22 @@ const baseUrlProveedor = AppSettings.API_ENDPOINT+ '/proveedor';
 export class ProveedorService {
 
   constructor(private http: HttpClient) { }
+
+  listaProveedor():Observable<Proveedor[]>{
+    return this.http.get<Proveedor[]>(baseUrlUtil +"/listaProveedor");
+  }
+
   registraProveedor(proveedor: Proveedor):Observable<any>{
     return this.http.post<any>(baseUrlProveedor, proveedor);
-}
+  }
+
+  listaProveedores(razonsocial: string, ruc:string, idUbigeo:number, estado:number){
+    const params = new HttpParams().set("razonsocial", razonsocial).set("ruc",ruc).set("idUbigeo",idUbigeo).set("estado",estado);
+    return this.http.get<any>(baseUrlProveedor+"/listaProveedorConParametros", {params});
+  }
+
+
+
+
   
 }
