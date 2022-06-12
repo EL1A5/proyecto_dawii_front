790fd4b452f08d82c2bd1fece3d5a7ff7f3e9314
EL1A5 <provinciaelias@gmail.com>
2022-06-12 13:52:12 -0500

    crud producto completo

--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -9,6 +9,7 @@ const baseUrlProducto = AppSettings.API_ENDPOINT+ '/producto';
 
 
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -24,6 +25,23 @@ export class ProductoService {
     const params = new HttpParams().set("nombre", nombre).set("serie", serie).set("idPais", idPais).set("estado", estado);  
     return this.http.get<any>(baseUrlProducto + "/listaProductosPorFiltros", {params});
  }
+
+ //CRUD
+ listaProductoFiltro(filtro:string):Observable<Producto[]> {
+  return this.http.get<Producto[]>(baseUrlProducto + "/listaProductosPorNombre/"+ filtro);
+}  
+
+//registraProducto(obj: Producto): Observable<any>{
+  //return this.http.post(baseUrlProducto+ "/registraProducto", obj);
+//}
+
+actualizaProducto(obj: Producto): Observable<any>{
+return this.http.put(baseUrlProducto + "/actualizaProducto", obj);
+}
+
+eliminaProducto(id: any): Observable<any>{
+return this.http.delete(baseUrlProducto + "/eliminaProducto/" + id);
+}
 
 }
 
