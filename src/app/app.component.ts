28c6d1aa838d6fce75f97f25f511d3c24a4037e3
EL1A5 <provinciaelias@gmail.com>
2022-07-03 12:25:49 -0500

    chatbot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,14 @@ export class AppComponent {
   constructor(http: HttpClient ){
 
   }
+
+  ngOnInit() {
+    (function(d, m){
+    var kommunicateSettings = {"appId":"1faaaec3e7961ef9d8af25666e2d0dd60","conversationTitle":"Bot"};
+    var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+    s.src = "https://api.kommunicate.io/v2/kommunicate.app";
+    var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+    (window as any).kommunicate = m; m._globals = kommunicateSettings;
+    })(document, (window as any).kommunicate || {});
+    }
 }
